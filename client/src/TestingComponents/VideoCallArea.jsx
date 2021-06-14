a2bc5fd9f8c01965b5eed7942be7f7bc996c0e12
yushsingla <yushsingla@gmail.com>
2021-06-15 04:07:47 +0530

    refined screen sharing and little refactoring

--- client/src/TestingComponents/VideoCallArea.jsx
+++ client/src/TestingComponents/VideoCallArea.jsx
@@ -19,43 +19,98 @@ export default function VideoCallArea() {
   useEffect(() => {
     setCameraStreaming();
   }, []);
-  async function setCameraStreaming() {
-    let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+  async function setCameraStreaming(callback) {
+    try {
+      let stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+      if (callback) callback();
+      connectedPeers.current = {};
+      socket.connect();
+      toggleAudio.current = () => {
+        stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
+        setAudio((prev) => !prev);
+      };
+      toggleVideo.current = () => {
+        console.log(stream.getAudioTracks()[0].enabled);
+        stream.getVideoTracks()[0].enabled = !stream.getVideoTracks()[0].enabled;
+        setVideo((prev) => !prev);
+      };
+      const myPeer = new Peer(undefined, {
+        // host: "peerjs-server.herokuapp.com",
+        // secure: true,
+        // port: 443,
+        host: "/",
+        port: "3001",
+      });
+      toggleShareScreen.current.start = () => {
+        setScreenShareStream(() => {
+          setSharingScreen(true);
+          stream.getTracks().forEach((track) => {
+            track.stop();
+          });
+          myPeer.disconnect();
+          const events = ["user-disconnected", "user-connected"];
+          events.forEach((event) => {
+            console.log(`turning off the event ${event}`);
+            socket.off(event);
+          });
+          socket.disconnect();
+        });
+      };
+      setUpSocketsAndPeerEvents({ socket, myPeer, stream });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async function setScreenShareStream(callback) {
+    try {
+      let AudioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      let ScreenShareStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
+      if (callback) callback();
+      connectedPeers.current = {};
+      let tracks = [];
+      tracks = tracks.concat(AudioStream.getAudioTracks());
+      tracks = tracks.concat(ScreenShareStream.getVideoTracks());
+      let stream = new MediaStream(tracks);
+      toggleAudio.current = () => {
+        stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
+        setAudio((prev) => !prev);
+      };
+      stream.getVideoTracks()[0].onended = () => {
+        setSharingScreen(false);
+        toggleShareScreen.current.stop();
+      };
+      console.log(stream);
+      console.log(tracks);
+      const myPeer = new Peer(undefined, {
+        // host: "peerjs-server.herokuapp.com",
+        // secure: true,
+        // port: 443,
+        host: "/",
+        port: "3001",
+      });
+      socket.connect();
+      toggleShareScreen.current.stop = () => {
+        setCameraStreaming(() => {
+          myPeer.disconnect();
+          stream.getTracks().forEach((track) => {
+            track.stop();
+          });
+          const events = ["user-disconnected", "user-connected"];
+          events.forEach((event) => {
+            console.log(`turning off the events ${event}`);
+            socket.off(event);
+          });
+          socket.disconnect();
+        });
+      };
+      setUpSocketsAndPeerEvents({ socket, myPeer, stream });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    connectedPeers.current = {};
-    socket.connect();
-    toggleAudio.current = () => {
-      stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
-      setAudio((prev) => !prev);
-    };
-    toggleVideo.current = () => {
-      console.log(stream.getAudioTracks()[0].enabled);
-      stream.getVideoTracks()[0].enabled = !stream.getVideoTracks()[0].enabled;
-      setVideo((prev) => !prev);
-    };
-    const myPeer = new Peer(undefined, {
-      // host: "peerjs-server.herokuapp.com",
-      // secure: true,
-      // port: 443,
-      host: "/",
-      port: "3001",
-    });
-    toggleShareScreen.current.start = () => {
-      stream.getTracks().forEach((track) => {
-        track.stop();
-      });
-      myPeer.disconnect();
-      const events = ["user-disconnected", "user-connected"];
-      events.forEach((event) => {
-        console.log(`turning off the event ${event}`);
-        socket.off(event);
-      });
-      socket.disconnect();
-      setScreenShareStream();
-    };
+  function setUpSocketsAndPeerEvents({ socket, myPeer, stream }) {
     myPeer.on("open", (userId) => {
-      // console.log("new peer has been created");
-      // console.log(myPeer);
       setMyId(userId);
       setVideos((prev) => {
         return [{ stream, userId }];
@@ -84,108 +139,12 @@ export default function VideoCallArea() {
       });
     });
     socket.on("user-disconnected", (userId) => {
-      console.log("disconnected", userId);
+      console.log("A user disconnected", userId);
 
       connectedPeers.current[userId].close();
       delete connectedPeers[userId];
     });
     myPeer.on("call", (call) => {
-      call.answer(stream);
-      call.on("stream", (userVideoStream) => {
-        if (connectedPeers.current[call.peer]) {
-          return;
-        }
-        connectedPeers.current[call.peer] = call;
-        addVideoStream(userVideoStream, call.peer);
-      });
-      call.on("close", () => {
-        setVideos((prev) => {
-          return prev.filter((video) => video.userId !== call.peer);
-        });
-      });
-    });
-    history.listen(() => {
-      if (history.action === "POP") {
-        stream.getTracks().forEach((track) => {
-          track.stop();
-        });
-        console.log(history);
-
-        myPeer.disconnect();
-        socket.disconnect();
-      }
-    });
-  }
-  async function setScreenShareStream() {
-    let AudioStream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    let ScreenShareStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true });
-    connectedPeers.current = {};
-    let tracks = [];
-    tracks = tracks.concat(AudioStream.getAudioTracks());
-    tracks = tracks.concat(ScreenShareStream.getVideoTracks());
-    let stream = new MediaStream(tracks);
-    toggleAudio.current = () => {
-      stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
-      setAudio((prev) => !prev);
-    };
-    stream.getVideoTracks()[0].onended = () => {
-      setSharingScreen(false);
-      toggleShareScreen.current.stop();
-    };
-    console.log(stream);
-    console.log(tracks);
-    const myPeer = new Peer(undefined, {
-      // host: "peerjs-server.herokuapp.com",
-      // secure: true,
-      // port: 443,
-      host: "/",
-      port: "3001",
-    });
-    socket.connect();
-    toggleShareScreen.current.stop = () => {
-      myPeer.disconnect();
-      stream.getTracks().forEach((track) => {
-        track.stop();
-      });
-      const events = ["user-disconnected", "user-connected"];
-      events.forEach((event) => {
-        console.log(`turning off the events ${event}`);
-        socket.off(event);
-      });
-      socket.disconnect();
-      setCameraStreaming();
-    };
-    myPeer.on("open", (userId) => {
-      const roomId = window.location.pathname.split("/")[2];
-      socket.emit("join-room", roomId, userId);
-      setVideos([{ userId, stream }]);
-      setMyId(userId);
-    });
-    socket.on("user-connected", (userId) => {
-      const call = myPeer.call(userId, stream);
-      if (!call) console.error("call is undefined");
-      call.on("stream", (userVideoStream) => {
-        if (connectedPeers.current[call.peer]) {
-          return;
-        }
-        connectedPeers.current[call.peer] = call;
-        addVideoStream(userVideoStream, call.peer);
-      });
-      call.on("close", () => {
-        setVideos((prev) => {
-          return prev.filter((video) => video.userId !== call.peer);
-        });
-      });
-    });
-    socket.on("user-disconnected", (userId) => {
-      console.log("disconnected", userId);
-      const events = ["user-disconnected", "user-connected"];
-
-      connectedPeers.current[userId].close();
-      delete connectedPeers[userId];
-    });
-    myPeer.on("call", (call) => {
-      console.log("I was called");
       call.answer(stream);
       call.on("stream", (userVideoStream) => {
         if (connectedPeers.current[call.peer]) {
@@ -233,7 +192,6 @@ export default function VideoCallArea() {
         onClick={() => {
           if (!sharingScreen) toggleShareScreen.current.start();
           else toggleShareScreen.current.stop();
-          setSharingScreen((prev) => !prev);
         }}
       >
         {sharingScreen ? "Stop Share" : "Share Screen"}

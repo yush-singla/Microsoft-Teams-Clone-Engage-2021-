5e1e07a15f558f6c50d4a2bd53e6edbc9401e88e
yushsingla <yushsingla@gmail.com>
2021-06-21 18:39:53 +0530

    now auth redirects work properly

--- client/src/utils/handleSignIn.js
+++ client/src/utils/handleSignIn.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 export default function handleSignIn(service, cameFrom, prev) {
-  const [extra, join, room] = cameFrom.split("/");
-  if (cameFrom && prev) window.open(`http://localhost:5000/auth/${service}?join=${join}&room=${room}&prev=${prev}`, "_self");
-  else if (cameFrom) window.open(`http://localhost:5000/auth/${service}?join=${join}&room=${room}`, "_self");
-  else window.open(`http://localhost:5000/auth/${service}`, "_self");
+  if (cameFrom) {
+    const [extra, join, room] = cameFrom.split("/");
+    if (prev) window.open(`http://localhost:5000/auth/${service}?join=${join}&room=${room}&prev=${prev}`, "_self");
+    else window.open(`http://localhost:5000/auth/${service}?join=${join}&room=${room}`, "_self");
+  } else window.open(`http://localhost:5000/auth/${service}`, "_self");
 }
